d58e81c1fc1470da267b1f88e422fc5f21e7fcb8
Adrián Fernández <adrian.ipod25@gmail.com>
2015-10-21 19:25:34 -0500

    C

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace EditDistance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPalabra1 = new System.Windows.Forms.TextBox();
+            this.txtPalabra2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(31, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(31, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Palabra 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(199, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Palabra 2:";
+            // 
+            // txtPalabra1
+            // 
+            this.txtPalabra1.Location = new System.Drawing.Point(31, 30);
+            this.txtPalabra1.Name = "txtPalabra1";
+            this.txtPalabra1.Size = new System.Drawing.Size(100, 20);
+            this.txtPalabra1.TabIndex = 3;
+            // 
+            // txtPalabra2
+            // 
+            this.txtPalabra2.Location = new System.Drawing.Point(202, 29);
+            this.txtPalabra2.Name = "txtPalabra2";
+            this.txtPalabra2.Size = new System.Drawing.Size(100, 20);
+            this.txtPalabra2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(224, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "La diferencia entre palabras es:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.AutoSize = true;
+            this.txtResultado.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.ForeColor = System.Drawing.Color.Black;
+            this.txtResultado.Location = new System.Drawing.Point(224, 113);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(0, 13);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 167);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPalabra2);
+            this.Controls.Add(this.txtPalabra1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Edit Distance";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPalabra1;
+        private System.Windows.Forms.TextBox txtPalabra2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtResultado;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EditDistance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Calcula el mínimo entre tres variables diferentes.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        private int min(int a, int b,int c)
+        {
+            return Math.Min(a, Math.Min(b, c));
+        }
+
+        /// <summary>
+        /// Método que mediante DP (programación dinámica)
+        /// calcula la diferencia entre una palabra y otra.
+        /// </summary>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns></returns>
+        private int Levenshtein(string p1, string p2)
+        {
+            int t1 = p1.Length;
+            int t2 = p2.Length;
+            int[,] dp = new int[1000, 1000];
+
+            for (int i = 0; i < t1; i++)
+                dp[i,0] = i;
+
+            for (int i = 0; i < t2; i++)
+                dp[0,i] = i;
+
+            for (int i=1;i<t1;i++)
+            {
+                for(int j=1;j<t2;j++)
+                {
+                    int cost = (p1[i] == p2[j]) ? 0 : 1;
+                    dp[i,j] = min(dp[i-1,j] + 1, dp[i, j-1] + 1, dp[i - 1, j - 1] + cost);
+                }
+            }
+            return dp[t1 - 1, t2 - 1];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int result = Levenshtein(txtPalabra1.Text,txtPalabra2.Text);
+            txtResultado.Text = result.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtPalabra1.Text = "";
+            txtPalabra2.Text = "";
+            txtResultado.Text = "";
+
+        }
+    }
+}
